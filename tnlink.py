4b51d6e3a84964d3232a8d98d20555f89f2d5d38
amitbiswas8514 <49386353+amitbiswas8514@users.noreply.github.com>
2022-12-29 12:23:00 +0530

    Update
    Update

--- tnlink.py
+++ tnlink.py
@@ -1,6 +1,7 @@
 import requests
 
-url = "https://tnlink.in/SCPI"  #@param {type:"string"}
+print("Enter Your tnlink url: ")
+url = input()  #@param {type:"string"}
 
 
 def tnlink(url):
@@ -8,8 +9,8 @@ def tnlink(url):
     DOMAIN = "https://gadgets.earnme.club"
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    download = requests.get(final_url, stream=True, allow_redirects=False)
+    download = requests.get(final_url, stream=True, allow_>
     return download.headers["location"]
-    
-# ---------------------------------------------------------------------------------------------------------------------
+
+# -------------------------------------------------------->
 print(tnlink(url))
